2b444fab9cfd768fadf8c10bb3082d0e3df31779
SPECKER Thibaut (PRESTA EXT) <SPECKETH@CDM.CM-CIC.FR>
2018-06-08 16:56:52 +0200

    Add ToolBar

--- app/src/main/java/com/tspecker/exadroid/WelcomeActivity.java
+++ app/src/main/java/com/tspecker/exadroid/WelcomeActivity.java
@@ -2,12 +2,20 @@ package com.tspecker.exadroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
+/**
+ * TODO auto-generated, fill with pertinent comment
+ * Created by SPECKETH on 08/06/2018.
+ */
 public class WelcomeActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+
+		Toolbar tb = findViewById(R.id.toolBar);
+		setSupportActionBar(tb);
 	}
 }
